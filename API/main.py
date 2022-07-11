29e634828304db61693fab83eabdfd20e5e78d90
aletts54 <41921682+aletts54@users.noreply.github.com>
2022-07-10 23:27:25 -0500

    Update main.py

--- API/main.py
+++ API/main.py
@@ -1,3 +1,4 @@
+#REST API with the Fast API Python library that displays a basic custom HTML webpage with 3 Post Methods to upload data into PostgreSQL and calculates weekly average trips by area or region using Apache Spark and Apache Sedona Geospatial Library.
 from typing import List
 from config_variables import *  #Config file with passwords and authentication
 import shutil
